e18392d8c261efaa7f178dcc4288d263a695f4bb
AdityaNair-jpg <adityanair807@gmail.com>
2025-05-20 20:36:27 +0530

    Update main.jsx

--- tailwindcss/src/main.jsx
+++ tailwindcss/src/main.jsx
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './epfrontend.jsx';
+
 import TopMenu from './topmenu.jsx';
 import AddTest from './addtest.jsx';
 import TestList from './TestList.jsx';
-import HistoryTab from './HistoryTab';
-import Login from './Login.jsx';
-import Register from './Register.jsx';
-import LandingPage from './LandingPage.jsx';
+import HistoryTab from './HistoryTab.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
